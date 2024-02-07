0c815fe6836087742f8113a78d1c9a680a3206b3
job nganga <jobugua97@gmail.com>
2024-02-07 18:56:29 +0300

    updated fiz buzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,7 @@
-# question 1: FizzBuzz
+#Question 1: FizzBuzz
+#Write a program that prints the numbers from 1 to 100. For multiples of 3, print "Fizz"; for
+#multiples of 5, print "Buzz"; and for numbers that are multiples of both 3 and 5, print
+#"FizzBuzz
 
 for number in range(1,101):
     if number%3 == 0:
