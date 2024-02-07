dcce2529a8773ccc891e011a6f18725517a1c5c9
job nganga <jobugua97@gmail.com>
2024-02-07 18:55:32 +0300

    capitalizing on a string

---
+++ Capitalizing.py
@@ -0,0 +1,9 @@
+#Question 4: Capitalize Words
+#Write a program that accepts a string as input, capitalizes the first letter of each word in the
+#string, and then returns the result string.
+
+def capitalizer(string_input):
+    String_output=string_input.title()
+    return String_output
+print(capitalizer("hi"))
+print(capitalizer("i love programming"))
