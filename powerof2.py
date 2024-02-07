97675a4672452dadbde82f2b4000d752dcb5d6a6
job nganga <jobugua97@gmail.com>
2024-02-07 18:57:11 +0300

    Finding Power

--- powerof2.py
+++ powerof2.py
@@ -1,4 +1,8 @@
-#finding if a number is  powers of 2
+#Question 3: Power of Two
+#Write a program that takes an integer as input and returns true if the input is a power of two.
+#Examples:
+#8=> returns true
+#6=> returns false
 def FindPower(number):
     count=1
     while True:
