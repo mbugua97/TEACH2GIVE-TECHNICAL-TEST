fbc32477de269da8ca7dbafae7cc28402bb663e0
job nganga <jobugua97@gmail.com>
2024-02-07 10:49:42 +0300

    fizzbuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+# question 1: FizzBuzz
+
+for number in range(1,101):
+    if number%3 == 0:
+        if number%5==0:
+            print("FizzFuzz")
+        else:
+            print("Fizz")
+    elif number%5==0:
+        print("Buzz")
+    else:
+        print(number)
+
