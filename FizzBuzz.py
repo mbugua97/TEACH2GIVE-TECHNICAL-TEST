40a856c50fe8a390c91451d624f4f0e79bb0b946
job nganga <jobugua97@gmail.com>
2024-02-08 16:38:52 +0300

    completed FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,10 +3,18 @@
 #multiples of 5, print "Buzz"; and for numbers that are multiples of both 3 and 5, print
 #"FizzBuzz
 
+
+""" My solution algorithm
+step1:iterating numbers through range 1-101
+step2:checking if the number is divisible by 3
+step3: if number is divisible by 3 check if is divisible by 5 and if true prints "fizz" if false print "Buzz"
+step4: check if number is divisible by 5 and if True prints Fizz
+step5: if step2 ,step3,step4 are false print the number
+"""
 for number in range(1,101):
     if number%3 == 0:
         if number%5==0:
-            print("FizzFuzz")
+            print("FizzBuzz")
         else:
             print("Fizz")
     elif number%5==0:
