f6a3becfeda6b076a7811443cf90139a9c8c0ac1
job nganga <jobugua97@gmail.com>
2024-02-08 17:05:41 +0300

    completed vowels

--- vowels.py
+++ vowels.py
@@ -2,6 +2,12 @@
 #Write a program that counts the number of vowels in a sentence.
 #eg " Hello World " => returns 2
 
+
+"""My solution algorith
+step1:have an array consiting of vowels
+step2: iterate though the sentence finding the vowels and append to a hold list
+step3: return count of distinct vowels found in step2 
+"""
 vowels=['a','e','i','o','u']
 def CountVowels(sentence):
     found_vowels=[]
