b1da859040bd61b234eb5afa0e02858647981f22
job nganga <jobugua97@gmail.com>
2024-02-08 16:57:30 +0300

    completed revese string

--- reverseint.py
+++ reverseint.py
@@ -6,6 +6,16 @@
 #For input -90, the program should return -9.
 #For input 91, the program should return 19.
 
+
+"""My Solution algorithm
+Step1:check if variable input is a positive value by checking if it is greater than 0
+step2: if step1 is true multiply my -1
+step3: converting the integer to a string and revese the string
+step4:converte the revesed string to an integer
+step5: if variable was a negative multipy by -1 else return variable
+
+"""
+
 def ReverseNumber(number):
     if number < 0:
        number*=-1
