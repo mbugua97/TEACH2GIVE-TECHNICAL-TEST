43e26fa9a6901214c85e55a6b7aa50bc35ceba11
job nganga <jobugua97@gmail.com>
2024-02-08 16:53:14 +0300

    completed power of 2

--- powerof2.py
+++ powerof2.py
@@ -3,6 +3,15 @@
 #Examples:
 #8=> returns true
 #6=> returns false
+
+"""My solution algorithm
+step1:iterate through the powers of 2
+step2:check if varable is equal to the iterate
+step3: if step 2 is true then the variable is a power of 2
+step4: if step 2 is false and the iterate  is greater than the varable return False
+
+"""
+
 def FindPower(number):
     count=1
     while True:
